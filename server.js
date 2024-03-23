57f44f27a2788063498d07190681855ef7b546f5
Fabian_Laptop <fb-224651@hs-weingarten.de>
2024-03-23 15:45:07 +0100

    Cors update

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Cors Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Middleware für JSON-Parsing
 app.use(express.json());
